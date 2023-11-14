618cf43eb9011830f9b479de2da83bc42ab9f1c9
Stella Maris González Robert <stegonyrob@yahoo.es>
2023-11-14 08:25:46 +0100

    change

---
+++ main.js
@@ -0,0 +1,14 @@
+import './src/pages/home/footer.js';
+import './src/pages/home/header.js';
+
+window.addEventListener('DOMContentLoaded', (event) => {
+
+  fetch('header.html')
+  .then(response => response.text())
+  .then(html => document.body.insertAdjacentHTML('beforeend', html));
+  
+
+  fetch('footer.html')
+  .then(response => response.text())
+  .then(html => document.body.insertAdjacentHTML('beforeend', html));
+});
